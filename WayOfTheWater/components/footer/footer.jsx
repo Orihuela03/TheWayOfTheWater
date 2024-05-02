29e45c798b2090096dd2f106058c95e738567e39
Orihuela03 <orihuelamarcos03@gmail.com>
2024-05-02 20:40:21 +0100

    Segunda Entrega

--- WayOfTheWater/components/footer/footer.jsx
+++ WayOfTheWater/components/footer/footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './footer.css';
 
+
 const Footer = () => {
   return (
     <footer className="footer">
@@ -8,8 +9,16 @@ const Footer = () => {
         &copy; {new Date().getFullYear()} Marcos Orihuela García.
       </p>
       <a className='link-format' href='https://formacion.intef.es/mod/book/view.php?id=2609&chapterid=2196'>Todos los derechos reservados.</a>
+      <script src="https://kit.fontawesome.com/1211bb8815.js" crossorigin="anonymous"></script>
+      <ul class="social-icons">
+        <li><a href="https://www.facebook.com/cifpsancristobal" target="_blank"><i class="fa-brands fa-square-facebook"></i></a></li>
+        <li><a href="https://twitter.com/cifpsancristobal" target="_blank"><i class="fa-brands fa-square-twitter"></i></a></li>
+        <li><a href="https://www.instagram.com/cifpsancrstoba" target="_blank"><i class="fa-brands fa-square-instagram"></i></a></li>
+      </ul>
+
     </footer>
   );
 };
+
 
 export default Footer;
