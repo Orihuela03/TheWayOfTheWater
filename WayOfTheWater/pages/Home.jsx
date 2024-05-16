a68c07b394ea2b2907b1cd19c89da06dac9b9ce0
Orihuela03 <orihuelamarcos03@gmail.com>
2024-05-16 20:38:47 +0100

    Esta es la tercera Entrega

--- WayOfTheWater/pages/Home.jsx
+++ WayOfTheWater/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Footer from "../components/footer/footer";
 import Header from "../components/header/header";
 import Content from "../components/content/content";
+
 import "./Home.css"
 
 function Home() {
